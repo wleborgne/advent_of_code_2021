d72af9e6ef3a3a20e15b55fe1f90c645a94a6a9a
Bill LeBorgne <bill@leborgne.name>
2021-12-03 07:53:02 -0700

    Day 1 Part 2
    Minor p1 refactor to pull input from parent directory; need to
    refactor later to take input filename from cmd line
    
    Take successive slices from input values, sum them, and compare
    to the previous sum. Start with a very large "previous" sum, and
    we don't care that the last few slices aren't 3 memmbers, as
    those sums will necessarily be smaller than the previous, and
    we are only counting increasing sums.

--- d1/p2/p2.rb
+++ d1/p2/p2.rb
@@ -9,16 +9,23 @@ file = File.open(input_file)
 data = file.read
 
 # Split contents into an array
-data_array = data.split
+# and convert elements to ints
+data_array = data.split.map { |x| x.to_i }
 
 # Iterate over array, counting adjacent increases
 # Don't forget to convert strings to ints!!!!
 count = 0
-data_array.each_with_index do |item, index|
-    next if index == 0
-    if item.to_i > data_array[index - 1].to_i
+prior = 999_999_999 # arbitrary really big value
+current = 0
+
+data_array.each_index do |i|
+    current = data_array.slice(i, 3).sum
+
+    if current > prior
         count += 1
     end
+
+    prior = current
 end
 
 puts count
