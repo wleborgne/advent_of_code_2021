ad1c77f41432f5e1b91543bb4a937c2309504961
Bill LeBorgne <bill@leborgne.name>
2021-12-03 08:26:28 -0700

    Day 2 Part 1
    I'll probably need to extract a bunch of this to a Submarine
    class if prior years are any indication.

---
+++ d2/p1/p1.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+# Open file
+input_file = "../input.txt"
+puts("Reading file #{input_file}")
+file = File.open(input_file)
+
+# Read file
+data = file.read.split("\n")
+
+# Initial position
+position = {x: 0, y:0}
+
+data.each do |command|
+    direction, displacement = command.split
+    displacement = displacement.to_i
+
+    puts("Moving #{direction} #{displacement} units.")
+    case direction
+    when "forward"
+        position[:x] += displacement
+    when "down"
+        position[:y] += displacement
+    when "up"
+        position[:y] -= displacement
+    end
+    puts("New position: x = #{position[:x]}, y = #{position[:y]}")
+end
+
+puts("Result: #{position[:x] * position[:y]}")
