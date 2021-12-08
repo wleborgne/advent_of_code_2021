764295b63545e2a297a21c09066f1c79b660b70f
Bill LeBorgne <bill@leborgne.name>
2021-12-08 08:08:21 -0700

    Day 5 Part 2
    Horizontal/vertical can probably be combined with the diagonal

---
+++ d5/p2/p2.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+# Open file
+input_file = "../input.txt"
+puts("Reading file #{input_file}")
+file = File.open(input_file)
+
+# Read file
+data = file.read.split("\n")
+
+# initialize array to hold counts
+points = Array.new(1000) { Array.new(1000, 0) }
+
+data.each do |line|
+    puts("Processing #{line}")
+    x1, y1 = line.split(" ").first.split(",").map { |n| n.to_i }
+    x2, y2 = line.split(" ").last.split(",").map { |n| n.to_i }
+    xs = [x1, x2].sort
+    ys = [y1, y2].sort
+
+    if (x1 == x2) || (y1 == y2)
+        puts("Marking horizontal/vertical...")
+        (xs.first..xs.last).each do |x|
+            (ys.first..ys.last).each do |y|
+                points[y][x] += 1
+            end
+        end
+    else
+        puts("Marking diagonal...")
+        offset = ys.last - ys.first
+        y_dir = y1 < y2 ? 1 : -1
+        x_dir = x1 < x2 ? 1 : -1
+        current_x = x1
+        current_y = y1
+        (0..offset).each do |_|
+            points[current_y][current_x] += 1
+            current_x += x_dir
+            current_y += y_dir
+        end
+    end
+end
+
+overlap = 0
+points.each do |row|
+    #puts row.join("")
+    row.each do |point|
+        if point > 1
+            overlap += 1
+        end
+    end
+end
+
+puts overlap
